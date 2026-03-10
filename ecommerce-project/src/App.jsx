582dbd2a0f1dc4a64a92a34071e1d7d32dcfe572
Temesgen Tesfahunegn <temesgenb627@gmail.com>
2026-03-10 15:52:54 -0400

    moved orders page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'React-router';
 import { HomePage } from './pages/HomePage'; // import the HomePage component that you created in another file
 import { CheckoutPage } from './pages/CheckoutPage'; 
+import { OrdersPage } from './pages/OrdersPage';
 import './App.css'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
-     </Routes>
+      <Route path="orders" element={<OrdersPage />} />
+      </Routes>
   ); 
 }
 
--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,5 +1,5 @@
 import './checkout-header.css';
-import 'CheckoutPage.css';
+import './CheckoutPage.css';
 
 export function CheckoutPage(){
     return (
