81ddd6820028872956d962ce47d66d739d9ac77d
Temesgen Tesfahunegn <temesgenb627@gmail.com>
2026-03-11 16:06:12 -0400

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {   // export it so that you can use it in another file
-fetch ('http://localhost:3000/api/products')
-    .then((response) => {
-        return response.json().
-        then((data) => {
-            console.log(data);
-        })
-    })
+    axios.get('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response.data);
+        });
     return (
         <>
             <Header />
