cd2c59004eab06b7730c3c6cabbb840ff81eafd1
Temesgen Tesfahunegn <temesgenb627@gmail.com>
2026-03-11 12:58:33 -0400

    dont know

---
+++ ecommerce-project/src/components/demo.jsx
@@ -0,0 +1,10 @@
+import { Header } from './Header';
+export function Demo(){
+    return(
+        <>
+            <Header />
+        <title>Demo</title>
+        <p>page not found</p>
+        </>
+    );
+}
