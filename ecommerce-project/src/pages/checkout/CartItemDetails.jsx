5d18fcc80309bb8dc8eadf5872d9b5a078a43b1a
Temesgen Tesfahunegn <temesgenb627@gmail.com>
2026-04-03 11:01:29 -0400

    updated the state of input textbox

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useState } from "react";
 export function CartItemDetails({ cartItem, loadCart }) {
     const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+    const [quantity, setQuantity] = useState(cartItem.quantity);
     const deleteCartItem = async () => {
         await axios.delete(`/api/cart-items/${cartItem.productId}`);
         await loadCart();
@@ -14,6 +15,11 @@ export function CartItemDetails({ cartItem, loadCart }) {
             } else {
                 setIsUpdatingQuantity(true);
             }
+        }
+
+        const updateInputQuantity = (event) => {  // parameter goes inside parenthesis of an arrow function
+            setQuantity(event.target.value);
+            // event.target.value is the new quantity that the user has typed in the input field. We need to update the quantity state with this new value.
         }
     return (
         <>
@@ -30,7 +36,7 @@ export function CartItemDetails({ cartItem, loadCart }) {
                     <span>
                         Quantity: {
                             isUpdatingQuantity ? 
-                            <input type="text" className="quantity-textbook" />:
+                            <input type="text" className="quantity-textbook" value={quantity} onChange={updateInputQuantity} />:
                             <span className="quantity-label">{cartItem.quantity}</span>
                         }</span>
                     <span className="update-quantity-link link-primary" onClick={updateQuantity}>
